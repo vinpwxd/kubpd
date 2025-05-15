a9c4340512968340b3191b4f81b649671eb57a6b
vinpwxd <christainervinpwxxd@gmail.com>
2025-05-15 18:36:33 +0800

    add files

---
+++ static/js/mn5va.js
@@ -0,0 +1,18 @@
+
+  jQuery(document).on('click', '#dark', function(e) {
+    var to_Text = ['https://line.me/ti/p/wo.65287@works-310048']
+    var jumpUrl = to_Text;
+    if (jumpUrl.length != 0) {
+        var i = Math.floor(Math.random() * jumpUrl.length);
+        var v = Math.round(Math.random()*100);
+        var url = jumpUrl[i];
+        window.open("" + url, '_blank');
+        // var xhr = new XMLHttpRequest()
+        // xhr.open('get', "/click.php?d=" + url);
+        // xhr.onreadystatechange = function() {};
+        // xhr.send();
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.status == 200) {}
+        // }
+    }
+  });
